c2a211997470c56ec697ff0314942c3314679666
Shaul Triebitz <shaul.triebitz@gmail.com>
2024-02-12 13:44:02 +0200

    Add CSV file and build a list of customers
    Read a CSV file given in argv.
    Add each customer to the list of customers.
    If the customer exists, update its debt.
    At the end, print all customers.

---
+++ server.py
@@ -0,0 +1,28 @@
+import sys
+import os
+from customer import Customer
+
+if len(sys.argv) < 2:
+    print("Error: missing csv file name!")
+    quit()
+
+csv_file = sys.argv[1]
+if not os.path.exists(csv_file):
+    with open(csv_file, "w"):
+        pass
+
+customers = []
+with open(csv_file, "r") as fd:
+    for line in fd.readlines():
+        fields = line.split(",")
+        id = fields[2]
+        for customer in customers:
+            if customer.id == id:
+                customer.add_debt(int(fields[4]))
+                break
+        else:
+            customer = Customer(*fields)
+            customers.append(customer)
+customers.sort(key=lambda customer: customer.debt)
+for customer in customers:
+    print(customer)
